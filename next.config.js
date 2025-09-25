32f7dc255e88a2af7fbea3d90fb360c8fb0b9bec
Dhruv Sharma <iamdhruvsharma3@gmail.com>
2025-09-25 11:30:18 +0530

    fix: changed next config file for cdn images

--- next.config.js
+++ next.config.js
@@ -18,6 +18,28 @@ const nextConfig = {
       },
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "wrapped-images.spotifycdn.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
